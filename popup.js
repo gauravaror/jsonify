111da5ba594a13dca5e939bea966b017b0986dcc
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-08-19 18:20:24 +0530

    make json string look pretty

--- popup.js
+++ popup.js
@@ -5,12 +5,23 @@ chrome.runtime.sendMessage({jsonify: "currentpage",url: window.href}, function(r
      checkStatus();
 });
 
+function replaceAll(find, replace, str) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
+function populateJson(element,jsonString) {
+    var tmp = jsonString.replace(/\\n/g,"<br>");
+    console.log(tmp);
+    var tmp2 = tmp.replace(/\\\"/g,"\"",tmp);
+    console.log(tmp2);
+    element.innerHTML  = tmp2;
+}
 function checkStatus(){
         chrome.runtime.sendMessage({getjson: "now",url: window.href},function(response) {
             console.log(response);
             if(response.status == "OK") { 
                 json = response.json;
-                document.getElementById("jsoncontent").textContent = JSON.stringify(json,undefined,4);
+                populateJson(document.getElementById("jsoncontent"),JSON.stringify(json,undefined,4));
                 console.log(json);
                 stat=true;
             } else if (response.status == "WAIT"){
