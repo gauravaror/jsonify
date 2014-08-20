55d7239f02998fd07120dcaba018b12eb6e6739a
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-08-20 17:28:23 +0530

    Added waiting spinner while waiting for results

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 var stat = false;
 var json = undefined;
+var spinner = undefined;
 chrome.runtime.sendMessage({jsonify: "currentpage",url: window.href}, function(response) {
      console.log(response);
      checkStatus();
@@ -10,6 +11,9 @@ function replaceAll(find, replace, str) {
 }
 
 function populateJson(element,jsonString) {
+    if(spinner) {
+        spinner.stop();
+    }
     var tmp = jsonString.replace(/\\n/g,"<br>");
     console.log(tmp);
     var tmp2 = tmp.replace(/\\\"/g,"\"",tmp);
@@ -29,3 +33,30 @@ function checkStatus(){
             }
         });
 }
+
+
+
+
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
+document.addEventListener("DOMContentLoaded", function(event) {
+    var target = document.getElementById('spinner');
+    spinner = new Spinner(opts).spin(target);
+//    target.appendChild(spinner.el);
+});
